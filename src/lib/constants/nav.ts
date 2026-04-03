452028b55c60206bd8269c62797a868aa51cab6e
laya-resort-hotel <layaresorthotel@gmail.com>
2026-04-03 12:37:33 +0700

    Delete src directory

--- src/lib/constants/nav.ts
+++
@@ -1,9 +0,0 @@
-import { RESIDENT_ROUTES } from "./routes";
-
-export const residentBottomNav = [
-  { label: "Home", to: RESIDENT_ROUTES.HOME },
-  { label: "Card", to: RESIDENT_ROUTES.CARD },
-  { label: "Points", to: RESIDENT_ROUTES.POINTS },
-  { label: "Benefits", to: RESIDENT_ROUTES.BENEFITS },
-  { label: "More", to: RESIDENT_ROUTES.MORE },
-];
